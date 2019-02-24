73646dfa39528dfbc97dd55ff9da9efb6affc5a1
unknown <dio-roman@mail.ru>
2019-02-24 16:15:34 +0300

    Updates

---
+++ precache-manifest.0a1d8469847b372f9be4c23bad8cfb7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "596dc45915daf3f0f695",
+    "url": "/magaz/static/js/runtime~main.596dc459.js"
+  },
+  {
+    "revision": "e8c8525771710fbb380b",
+    "url": "/magaz/static/js/main.e8c85257.chunk.js"
+  },
+  {
+    "revision": "860fdd198102ede9dcc2",
+    "url": "/magaz/static/js/1.860fdd19.chunk.js"
+  },
+  {
+    "revision": "e8c8525771710fbb380b",
+    "url": "/magaz/static/css/main.b1f741a3.chunk.css"
+  },
+  {
+    "revision": "945513e7cce1275aef91eb0bd62d26dc",
+    "url": "/magaz/index.html"
+  }
+];
